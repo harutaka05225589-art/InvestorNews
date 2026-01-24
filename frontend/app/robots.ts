a64d2f1d975bb707a1072590a38fd81457532211
Haruya <harutaka05225589@gmail.com>
2026-01-24 16:30:52 +0900

    Add robots.ts to point to sitemap

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
+            disallow: ['/admin/', '/settings/', '/api/'],
         },
         sitemap: 'https://rich-investor-news.com/sitemap.xml',
     };
